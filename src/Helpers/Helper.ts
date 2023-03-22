caea300f93b8dac05894cca5a8b1f0541d6c4dac
João Lenon <lenonSec7@gmail.com>
2023-03-22 12:13:59 -0300

    fix: use scripts to build, clean and publish the code

--- src/Helpers/Helper.ts
+++ src/Helpers/Helper.ts
@@ -1,10 +1,16 @@
 import { SomeException } from '#src/Exceptions/SomeException'
 
 export class Helper {
+  /**
+   * Log hello world message.
+   */
   public static log() {
     console.log('hello world!')
   }
 
+  /**
+   * Throw an exception.
+   */
   public static exception() {
     throw new SomeException('something went wrong!')
   }
